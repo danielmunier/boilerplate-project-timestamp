44444bc3a200456b0a59897e291ead2d9a5a8684
Daniel <dmunier27@gmail.com>
2024-01-16 15:59:13 -0300

    First commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -19,14 +19,83 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+
+
+
+
+app.get("/api/:date?", function (req, res) {
+
+  try {
+    let date = req.params.date
+
+    if (!date) {
+      const date = new Date()
+      const dateToUnix = Math.floor(date.getTime() / 1000)
+      let data = {
+        unix: dateToUnix,
+        utc: date.toUTCString()
+      }
+      return res.json(data)
+
+    }
+
+
+
+    if (!isNaN(date)) {
+      // UNIX
+
+  
+      let dateObject = new Date(parseInt(date))
+     
+
+      let data = {
+        unix: req.params.date,
+        utc: dateObject.toUTCString(),
+      }
+
+      return res.json(data);
+
+    } 
+    
+    else if (Date.parse(date)) {
+      // If its a number and the parse method can return a timestamp
+
+
+      const parts = date.split("-")
+
+      let unixToDate = new Date(`${parts[0]}-${parts[1]}-${parts[2]}`)
+      let dateToUnix = Math.floor(unixToDate.getTime() / 1000)
+
+      let data = {
+        unix: dateToUnix,
+        utc: unixToDate.toUTCString()
+      }
+
+      return res.json(data);
+
+    } else {
+      res.json({ error: "Invalid Date" })
+    }
+
+  } catch (e) {
+    res.status(400)
+  }
+
+
+
+  /* 
+    let hours = date.getHours()
+    let minutes = date.getMinutes()
+    let seconds =  date.getSeconds()
+   */
+
+
 });
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(3000, function () {
+  console.log('Your app is listening on port:' + " http://localhost:3000");
 });
